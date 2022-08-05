8a884e0771e109bbf4d6de9182e4624ed8044274
Thiago Colares <thiago.colares@shopify.com>
2022-08-05 08:26:27 -0400

    Update nutrition

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :nutrition_update, mutation: Mutations::NutritionUpdate
     field :food_update, mutation: Mutations::FoodUpdate
     field :nutrition_create, mutation: Mutations::NutritionCreate
     field :food_create, mutation: Mutations::FoodCreate

eea8bb7d18f0021331b811b17e9d1613964ae3c7
Thiago Colares <thiago.colares@shopify.com>
2022-08-05 08:38:25 -0400

    Delete nutrition

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,7 @@
 module Types
   class MutationType < Types::BaseObject
+    field :nutrition_delete, mutation: Mutations::NutritionDelete
+    field :food_delete, mutation: Mutations::FoodDelete
     field :nutrition_update, mutation: Mutations::NutritionUpdate
     field :food_update, mutation: Mutations::FoodUpdate
     field :nutrition_create, mutation: Mutations::NutritionCreate
